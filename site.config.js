9c12aebaafca81151248929784f25e73451bae6f
pengyuyan666666 <144613798+pengyuyan666666@users.noreply.github.com>
2024-06-03 17:30:32 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -5,7 +5,7 @@ const CONFIG = {
     image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "frontend developer",
     bio: "I develop everything using node.",
-    email: "hello@spike.ac.cn",
+    email: "pengyuyan_@outlook.com",
     linkedin: "morethanmin",
     github: "morethanmin",
     instagram: "",
